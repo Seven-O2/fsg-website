06924c477b5abca00b015cb03eed2b2dd3d09ba1
Joel Allemann <joel.allemann99@gmail.com>
2024-11-01 22:54:58 +0100

    initial

---
+++ js/functions.js
@@ -0,0 +1,67 @@
+// Passed elements follow the mouse
+const FollowMouse = (elements) => {
+    let imageXOffset = -0.5;
+    let imageYOffset = -0.5;
+
+    let posXOld = 0;
+    let posYOld = 0;
+
+    document.addEventListener("mousemove", e => {
+        const deltaX = (e.clientX - posXOld) / 500
+        const deltaY = (e.clientY - posYOld) / 500
+      
+        posXOld = e.clientX;
+        posYOld = e.clientY;
+        
+        imageXOffset = Math.min(0, Math.max(-1, imageXOffset + deltaX));
+        imageYOffset = Math.min(0, Math.max(-1, imageYOffset + deltaY));
+
+        elements.forEach(element => {
+            element.style.left = imageXOffset + "vw"; 
+            element.style.top  = imageYOffset + "vw";
+        });
+    }, false);
+}
+
+// Passed elements follow scroll
+const FollowScrollInverse = (elements) => {
+    document.addEventListener("scroll", _ => {
+        const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+        elements.forEach(element => {
+            element.style.top = -1 * scrollPosition / 2 + "px";
+        });
+    }, false);
+}
+
+const SectionScrollActions = (elements) => {
+    const defaultSpacing = getComputedStyle(document.documentElement).getPropertyValue("--default-spacing").slice(0, -2);
+    const delay = 20;
+    document.addEventListener("scroll", _ => {
+        Array.from(elements).forEach(element => {
+            const title = element.getElementsByClassName("section-title")[0];
+            const content = element.getElementsByClassName("section-content")[0];
+
+            const offsetFromCenter = window.innerHeight / 2 - title.getBoundingClientRect().top - delay; // represents the offset of the sections start to the screens center
+
+            // Rotate title to left side plus parallax 
+            title.style.rotate    = Math.min( 90, Math.max(0,                         offsetFromCenter / 1.5)) + "deg";
+            title.style.fontSize  = Math.min( 16, Math.max(defaultSpacing * 0.8, 16 - offsetFromCenter / 11)) + "vw";
+            title.style.marginTop = Math.max(0, offsetFromCenter / 4) + "px";
+            
+            // Shift content to title position
+            content.style.marginTop = Math.max(0, Math.min(21, 21 - offsetFromCenter / 6.25)) + "vw";
+        });
+    }, false);
+}
+
+const switchElements = (e1, e2) => {
+    element1 = document.getElementById(e1);
+    element2 = document.getElementById(e2);
+    if(element1.style.display !== "none") {
+        element2.style.display = element1.style.display;
+        element1.style.display = "none"
+    } else {
+        element1.style.display = element2.style.display;
+        element2.style.display = "none"
+    }
+}
