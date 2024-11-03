d527f8928124732606186bae7a6f855c47d79c51
Joel Allemann <joel.allemann99@gmail.com>
2024-11-03 18:02:03 +0100

    Adds funcup image and fixes 'cancelled' message

--- js/functions.js
+++ js/functions.js
@@ -23,7 +23,7 @@ const FollowMouse = (elements) => {
     }, false);
 }
 
-// Passed elements follow scroll
+// Passed elements follow scroll in an inverse manner
 const FollowScrollInverse = (elements) => {
     document.addEventListener("scroll", _ => {
         const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
@@ -33,26 +33,22 @@ const FollowScrollInverse = (elements) => {
     }, false);
 }
 
-const SectionScrollActions = (elements) => {
-    const defaultSpacing = getComputedStyle(document.documentElement).getPropertyValue("--default-spacing").slice(0, -2);
+// When scrolled past middle of the display, the element will be reduced in size
+const ReduceSizeOnScroll = (elements) => {
+    console.log(elements)
     const delay = 20;
     document.addEventListener("scroll", _ => {
         Array.from(elements).forEach(element => {
-            const title = element.getElementsByClassName("section-title")[0];
-            const offsetFromCenter = window.innerHeight / 2 - title.getBoundingClientRect().top - delay; // represents the offset of the sections start to the screens center
-            title.style.fontSize  = Math.min( 16, Math.max(defaultSpacing * 0.8, 16 - offsetFromCenter / 11)) + "vw";
+            const offsetFromCenter = window.innerHeight / 2 - element.getBoundingClientRect().top - delay; // represents the offset of the sections start to the screens center
+            element.style.fontSize = Math.min(8, Math.max(2, 8 - offsetFromCenter / 20)) + "rem";
         });
     }, false);
 }
 
-const switchElements = (e1, e2) => {
-    element1 = document.getElementById(e1);
-    element2 = document.getElementById(e2);
-    if(element1.style.display !== "none") {
-        element2.style.display = element1.style.display;
-        element1.style.display = "none"
-    } else {
-        element1.style.display = element2.style.display;
-        element2.style.display = "none"
-    }
+const CreateDates = (parent) => {
+
+}
+
+const CreateRankings = (parent) => {
+    
 }
